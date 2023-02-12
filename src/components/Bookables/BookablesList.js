6b738cd14a42ebbe6b0dd3a8d514cc22b974d38c
chenrenliang <474571921@qq.com>
2023-02-12 11:37:58 +0000

    feat: add userlist

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import { bookables } from "../../static.json";
 
 export default function BookablesList() {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter((b) => b.group === group);
 
-  let bookableIndex = 1;
+  const [bookableIndex, setBookableIndex] = useState(1);
 
   function changeBookable(selectedIndex) {
     bookableIndex = selectedIndex;
@@ -16,7 +17,7 @@ export default function BookablesList() {
       {bookablesInGroup.map((b, i) => {
         return (
           <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-            <button className="btn" onClick={() => changeBookable(i)}>
+            <button className="btn" onClick={() => setBookableIndex(i)}>
               {b.title}
             </button>
           </li>
