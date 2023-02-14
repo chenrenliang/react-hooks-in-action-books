7d23d9b8456ea7bb5bd541276366ed1944df8b94
chenrenliang <474571921@qq.com>
2023-02-14 22:40:29 +0800

    reducer

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,28 +1,49 @@
-import { useState } from "react";
+import { useReducer, Fragment } from "react";
 import { bookables, sessions, days } from "../../static.json";
 import { FaArrowRight } from "react-icons/fa";
 
-export default function BookablesList() {
-  const [group, setGroup] = useState("Kit");
-  const bookablesInGroup = bookables.filter((b) => b.group === group);
+import reducer from "./reducer";
 
-  const [bookableIndex, setBookableIndex] = useState(0);
+const initialState = {
+  group: "Rooms",
+  bookableIndex: 0,
+  hasDetails: true,
+  bookables
+};
+
+export default function BookablesList() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { group, bookableIndex, bookables, hasDetails } = state;
+
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
+  const bookable = bookablesInGroup[bookableIndex];
   const groups = [...new Set(bookables.map((b) => b.group))];
 
-  const bookable = bookablesInGroup[bookableIndex];
-  const [hasDetails, setHasDetails] = useState(false);
-
-  function nextBookable() {
-    setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
+  function changeGroup(e) {
+    dispatch({
+      type: "SET_GROUP",
+      payload: e.target.value
+    });
   }
 
-  function changeGroup(event) {
-    setGroup(event.target.value);
-    setBookableIndex(0);
+  function changeBookable(selectedIndex) {
+    dispatch({
+      type: "SET_BOOKABLE",
+      payload: selectedIndex
+    });
+  }
+
+  function nextBookable() {
+    dispatch({ type: "NEXT_BOOKABLE" });
+  }
+
+  function toggleDetails() {
+    dispatch({ type: "TOGGLE_HAS_DETAILS" });
   }
 
   return (
-    <>
+    <Fragment>
       <div>
         <select value={group} onChange={changeGroup}>
           {groups.map((g) => (
@@ -31,19 +52,15 @@ export default function BookablesList() {
             </option>
           ))}
         </select>
+
         <ul className="bookables items-list-nav">
-          {bookablesInGroup.map((b, i) => {
-            return (
-              <li
-                key={b.id}
-                className={i === bookableIndex ? "selected" : null}
-              >
-                <button className="btn" onClick={() => setBookableIndex(i)}>
-                  {b.title}
-                </button>
-              </li>
-            );
-          })}
+          {bookablesInGroup.map((b, i) => (
+            <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+              <button className="btn" onClick={() => changeBookable(i)}>
+                {b.title}
+              </button>
+            </li>
+          ))}
         </ul>
         <p>
           <button className="btn" onClick={nextBookable} autoFocus>
@@ -52,6 +69,7 @@ export default function BookablesList() {
           </button>
         </p>
       </div>
+
       {bookable && (
         <div className="bookable-details">
           <div className="item">
@@ -62,14 +80,15 @@ export default function BookablesList() {
                   <input
                     type="checkbox"
                     checked={hasDetails}
-                    onChange={() => setHasDetails((has) => !has)}
+                    onChange={toggleDetails}
                   />
-                  show details
+                  Show Details
                 </label>
               </span>
             </div>
 
             <p>{bookable.notes}</p>
+
             {hasDetails && (
               <div className="item-details">
                 <h3>Availability</h3>
@@ -79,7 +98,6 @@ export default function BookablesList() {
                       <li key={d}>{days[d]}</li>
                     ))}
                   </ul>
-
                   <ul>
                     {bookable.sessions.map((s) => (
                       <li key={s}>{sessions[s]}</li>
@@ -91,6 +109,6 @@ export default function BookablesList() {
           </div>
         </div>
       )}
-    </>
+    </Fragment>
   );
 }
